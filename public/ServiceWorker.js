92b4bdb9d51e3d43a99346fc6582ef30bb949f7e
SmallCodee <hannesasd@gmail.com>
2021-01-28 20:33:41 +0100

    Added darkmode

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,13 +1,16 @@
-
 const FILES_TO_CACHE = [
     "/manifest.json",
     "/offline",
+    "/resources/css/base.css",
     "/resources/css/nav_style.css",
+    "/resources/css/darkmode.css",
+    "/resources/css/forms.css",
     "/resources/css/offline_page.css",
     "/assets/images/logo.png",
     "/assets/images/splashscreen.png",
     "/assets/images/maskable_logo.png",
-    "https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;600;700&display=swap"
+    "https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;600;700&display=swap",
+    "https://code.jquery.com/jquery-3.5.1.min.js"
 ];
 
 const STATIC_CACHE_NAME = "static";
@@ -48,17 +51,37 @@ self.addEventListener('activate', e => {
 self.addEventListener("fetch", e => {
 
     e.respondWith(
+      
+      fetch(e.request)
+      .then((res) => {
 
-        caches.match(e.request).then(cache => {
+        const url = new URL(e.request.url);       
 
-          return cache || fetch(e.request).then(response => {
+        //Update cache if files come from local storage
+        if(url && url.pathname)
+          if(FILES_TO_CACHE.includes(url.pathname))
+            caches.open(STATIC_CACHE_NAME).then(function(cache) {
+              cache.put(url.pathname, res.clone());
+            });
 
-              return response;
+        return res.clone();
+
+      })
+      .catch(() => {
+
+        return caches.match(e.request.url, {ignoreVary: true})
+          .then(cache => {
+                  
+            if(cache)
+              return cache;
+            else
+              //Show offline page
+              return caches.match('offline', {ignoreVary: true});
 
           });
 
-        }).catch(() => caches.match('offline'))
-
-      );
+      })
+      
+    );
 
 });
